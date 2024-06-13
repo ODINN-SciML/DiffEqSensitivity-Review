208fd8ba0804e65084bc1d3dcdb18e782f9879c6
Avik Pal <avikpal@mit.edu>
2024-06-12 18:44:33 -0700

    Add a 2nd layer for testset

--- code/run_all_scripts.jl
+++ code/run_all_scripts.jl
@@ -1,4 +1,4 @@
-using SafeTestsets
+using SafeTestsets, Test
 
 function recursive_readdir!(cur_dir, path, list_of_scripts)
     fullpath = joinpath(cur_dir, path)
@@ -7,14 +7,16 @@ function recursive_readdir!(cur_dir, path, list_of_scripts)
             recursive_readdir!(fullpath, entry, list_of_scripts)
         end
     end
-    endswith(path, ".jl") && push!(list_of_scripts, fullpath)
+    endswith(path, ".jl") && (fullpath != @__FILE__) && push!(list_of_scripts, fullpath)
     return
 end
 
 scripts = []
 recursive_readdir!(@__DIR__, "", scripts)
 
-for script in scripts
-    @info "Running $script"
-    @time @eval @safetestset $script begin include($script) end
+@testset "DiffEqSensitivity Review All Scripts" begin
+    for script in scripts
+        @info "Running $script"
+        @time @eval @safetestset $script begin include($script) end
+    end
 end
